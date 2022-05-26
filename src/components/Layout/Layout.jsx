3a59ac755d186a19b059daecf13b7a7445f9ca5a
phamtuankhoinodejs <huynhanhpham734@gmail.com>
2022-05-26 10:30:10 +0700

    import router for client

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Routers from "../../router/Routers";
+
+
+const Layout = () => {
+    return (
+        <div>cq
+            <Routers />
+        </div>
+    )
+}
+
+export default Layout;
---
+++ src/router/Routers.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+
+
+const Routers = () =>{
+    return(
+        <Routes>
+            <Route path="/" />
+        </Routes>
+    )
+}
+
+export default Routers;
